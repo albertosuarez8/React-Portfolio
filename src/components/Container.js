67b13869d5f1288eb0adee7e2fa110324a0f70cc
Albert <alberto.suarez8@outlook.com>
2023-02-26 13:26:49 -0500

    Added Title to Container

--- src/components/Container.js
+++ src/components/Container.js
@@ -12,23 +12,28 @@ const testing = {
 function Container() {
     return (
         <>
-        <main>
-            <div className="p-16 sm:p-14">
-                <h1 className="text-[6rem] 2xl:text-9xl text-zinc-300">Alberto Suarez</h1>
-                <h2 className="text-2xl sm:text-lg text-zinc-300 sm:p-3">Business Analyst & Full Stack Student Developer.
-                </h2>
+            <main>
+                <div className="p-16 sm:p-14">
+                    <h1 className="text-[6rem] 2xl:text-9xl text-zinc-300">Alberto Suarez</h1>
+                    <h2 className="text-2xl sm:text-lg text-zinc-300 sm:p-3">Business Analyst & Full Stack Student Developer.
+                    </h2>
+                </div>
+            </main>
+            <div>
+                <div className='w-full flex justify-center text-zinc-300 text-[2.5rem]'> 
+                My Projects
+                </div>
+                <div id='container' className='flex items-center flex-col overflow-y-scroll w-[50vw] h-[50vh]'>
+                    <Card image={testing.image} bio={testing.bio} captionone={testing.captionone} captiontwo={testing.captiontwo} />
+                    <Card image={testing.image} bio={testing.bio} captionone={testing.captionone} captiontwo={testing.captiontwo} />
+                    <Card image={testing.image} bio={testing.bio} captionone={testing.captionone} captiontwo={testing.captiontwo} />
+                    <Card image={testing.image} bio={testing.bio} captionone={testing.captionone} captiontwo={testing.captiontwo} />
+                    <Card image={testing.image} bio={testing.bio} captionone={testing.captionone} captiontwo={testing.captiontwo} />
+                    <Card image={testing.image} bio={testing.bio} captionone={testing.captionone} captiontwo={testing.captiontwo} />
+                    <Card image={testing.image} bio={testing.bio} captionone={testing.captionone} captiontwo={testing.captiontwo} />
+                    <Card image={testing.image} bio={testing.bio} captionone={testing.captionone} captiontwo={testing.captiontwo} />
+                </div>
             </div>
-        </main>
-        <div id='container' className='flex items-center flex-col overflow-y-scroll w-[50vw] h-[50vh]'>
-        <Card image={testing.image} bio={testing.bio} captionone={testing.captionone} captiontwo={testing.captiontwo}/>
-        <Card image={testing.image} bio={testing.bio} captionone={testing.captionone} captiontwo={testing.captiontwo}/>
-        <Card image={testing.image} bio={testing.bio} captionone={testing.captionone} captiontwo={testing.captiontwo}/>
-        <Card image={testing.image} bio={testing.bio} captionone={testing.captionone} captiontwo={testing.captiontwo}/>
-        <Card image={testing.image} bio={testing.bio} captionone={testing.captionone} captiontwo={testing.captiontwo}/>
-        <Card image={testing.image} bio={testing.bio} captionone={testing.captionone} captiontwo={testing.captiontwo}/>
-        <Card image={testing.image} bio={testing.bio} captionone={testing.captionone} captiontwo={testing.captiontwo}/>
-        <Card image={testing.image} bio={testing.bio} captionone={testing.captionone} captiontwo={testing.captiontwo}/>
-        </div>
         </>
     );
 }
