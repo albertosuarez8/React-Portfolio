84202676a9fcd8e781df1a34c79397c8580b4ae5
Albert <alberto.suarez8@outlook.com>
2023-02-26 17:39:12 -0500

    Created Bio Component

---
+++ src/components/Bio.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Bio(props) {
+    return (
+        <>
+        <figure className="md:flex bg-zinc-300 rounded-xl p-8 md:p-0 dark:bg-slate-800 opacity-75 w-11/12 mb-8">
+            <img className=" w-24 h-24 md:w-48 md:h-auto rounded-[5px] mx-auto" src={props.image} alt="" width="384" height="512"/>
+                <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+                    <blockquote>
+                        <p className="text-lg font-medium">
+                            {props.bio}
+                        </p>
+                    </blockquote>
+                    <figcaption className="font-medium">
+                        <div className="text-sky-500 dark:text-sky-400">
+                            {props.captionone}
+                        </div>
+                        <div className="text-slate-700 dark:text-slate-500">
+                            {props.captiontwo}
+                        </div>
+                    </figcaption>
+                </div>
+        </figure>
+        </>
+    );
+}
+
+export default Bio;
--- src/components/Container.js
+++ src/components/Container.js
@@ -2,12 +2,21 @@ import React from 'react'
 import image from '../assets/glub.png'
 import Card from './Card';
 import Form from './Form';
+import Bio from './Bio';
+import alberto from '../assets/alberto.jpg'
 
 const testing = {
     image: image,
     bio: "Testing",
     captionone: "One",
     captiontwo: "Two"
+}
+
+const bio = {
+    image: alberto,
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet consectetur adipiscing elit duis tristique sollicitudin. Ut lectus arcu bibendum at varius vel pharetra.",
+    captionone: "",
+    captionone: "",
 }
 
 
@@ -26,12 +35,12 @@ function Container(props) {
                     <div className='w-full flex justify-center text-zinc-300 text-[2.5rem]'>
                         {props.title}
                     </div>
-                    <div id='container' className='flex items-center flex-col overflow-y-scroll w-[50vw] h-[50vh]'>
+                    <div id='container' className={`flex items-center flex-col overflow-y-scroll w-[50vw] h-[50vh] ${props.title === 'About Me' ? 'justify-center' : ''}`}>
                         <>
                             {(() => {
                                 switch (props.title) {
                                     case 'About Me':
-                                        return (<Card image={testing.image} bio={testing.bio} captionone={testing.captionone} captiontwo={testing.captiontwo} />);
+                                        return (<Bio image={bio.image} bio={bio.bio} captionone={bio.captionone} captiontwo={bio.captiontwo} />);
                                     case 'My Projects':
                                         return (
                                             <>
